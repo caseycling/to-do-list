7f8117137a7962d74281316592dbffcd0b65c791
Casey <caseyclinga@gmail.com>
2020-01-09 22:51:05 -0800

    Passed down delete functionality via props

--- src/Components/TodoItem.js
+++ src/Components/TodoItem.js
@@ -4,14 +4,13 @@ import PropTypes from 'prop-types';
 class TodoItem extends Component {
 
   getStyle = () => {
-    if (this.props.todo.completed) {
-      return {
-        background: '#f4f4f4',
-        padding: '10px',
-        borderBottom: '1px #ccc dotted',
-        textDecoration: this.props.todo.completed ?
-          'line-through' : 'none'
-      }
+    return {
+      background: '#f4f4f4',
+      padding: '10px',
+      borderBottom: '1px #ccc dotted',
+      textDecoration: this.props.todo.completed ?
+        'line-through' : 'none'
+
     }
   }
 
@@ -21,10 +20,12 @@ class TodoItem extends Component {
     return (
       <div style={this.getStyle()}>
         <p>
-          <input type="checkbox" onChange={this.props.markComplete.bind 
-          (this, id)
+          <input type="checkbox" onChange={this.props.markComplete.bind
+            (this, id)
           } /> {' '}
-          { title }</p>
+          {title}
+          <button onClick={this.props.delTodo.bind(this, id)} style={btnStyle}>X</button>
+        </p>
       </div>
     )
   }
@@ -34,6 +35,15 @@ TodoItem.propTypes = {
   todo: PropTypes.object.isRequired
 }
 
+const btnStyle = {
+  background: '#ff0000',
+  color: '#fff',
+  border: 'none',
+  padding: '5px 9px',
+  borderRadius: '50%',
+  cursor: 'pointer',
+  float: 'right'
+}
 
 
 export default TodoItem;
