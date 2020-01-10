7595ce4e21087451ac39cd6f89f7f8849cd72dfd
Casey <caseyclinga@gmail.com>
2020-01-09 17:27:11 -0800

    Created state

--- src/App.js
+++ src/App.js
@@ -4,6 +4,26 @@ import Todos from './Components/Todos'
 import './App.css';
 
 class App extends Component {
+  state = {
+    todos: [ 
+      {
+        id: 1,
+        title: 'Master React.js',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Clean bathroom',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Get grocceries',
+        completed: false
+      }
+    ]
+  }
+
   render() {
     return (
       <div className="App">
