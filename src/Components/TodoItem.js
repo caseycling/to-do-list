9e76c1c527f8252e5a008d53d67e5879c2148628
Casey <caseyclinga@gmail.com>
2020-01-09 21:06:10 -0800

    Added styling for completed tasks

--- src/Components/TodoItem.js
+++ src/Components/TodoItem.js
@@ -2,17 +2,34 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class TodoItem extends Component {
+
+  getStyle = () => {
+    if (this.props.todo.completed) {
+      return {
+        background: '#f4f4f4',
+        padding: '10px',
+        borderBottom: '1px #ccc dotted',
+        textDecoration: this.props.todo.completed ?
+          'line-through' : 'none'
+      }
+    }
+  }
+
+
   render() {
+    console.log(this.props.todo)
     return (
-        <div>
-            <p>{this.props.todo.title}</p>
-        </div>
+      <div style={this.getStyle()}>
+        <p>{this.props.todo.title}</p>
+      </div>
     )
   }
 }
 
 TodoItem.propTypes = {
-    todo: PropTypes.object.isRequired
-  }
+  todo: PropTypes.object.isRequired
+}
+
+
 
 export default TodoItem;
